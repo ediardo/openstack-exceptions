df83bbf5b032f8f77de9aaca8c6d379a6aeba5ea
Revon Mathews <revon.mathews@intel.com>
2016-09-28 23:33:04 -0500

    Created initial UI, rendered tables from json(keystone,cinder,nova) into corresponding tabs. Also implemented live filtering of error messages.

---
+++ app/app.js
@@ -0,0 +1,54 @@
+(function () {
+
+    var app = angular.module("OSExceptions", []);
+
+    app.controller('tabController', function(){
+        this.tab = 'keystone';
+        this.setTab = function(tab){
+            this.tab = tab;
+        };
+        this.isSet = function(checkTab){
+          return this.tab == checkTab;
+        };
+    });
+
+    app.controller('serviceController', ['$http', function ($http) {
+        var service = this;
+        service.name = [];
+
+        $http.get('data/keystone.json').success(function (data) {
+            service.name['keystone'] = data.keystone;
+        });
+        $http.get('data/nova.json').success(function (data) {
+            service.name['nova'] = data.nova;
+        });
+        $http.get('data/cinder.json').success(function (data) {
+            service.name['cinder'] = data.cinder;
+        });
+
+        this.getService = function (tab) {
+            return service.name[tab];
+        }
+        this.getMessages = function () {
+
+        };
+
+    }]);
+
+    // app.directive('serviceTable', ['$http', function($http){
+    //    return {
+    //        restrict : "E",
+    //        templateUrl : "html-components/service-table.html",
+    //        controller: function($http){
+    //            var service = this;
+    //            service.name = [];
+    //
+    //            $http.get('data/keystone.json').success(function (data) {
+    //                 service.name = data.keystone;
+    //            });
+    //        },
+    //        controllerAs: 'service'
+    //    };
+    // }]);
+
+})();
