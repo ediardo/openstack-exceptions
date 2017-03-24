ca6b3320b6decc14bb728ea0721005f13e3e0a76
Nish Patwa <nish.patwa@intel.com>
2017-03-24 05:08:01 +0000

    Added cinder repo parser

--- gitcrawler/parser_cinder.py
+++ gitcrawler/parser_cinder.py
@@ -5,63 +5,67 @@ import json
 import inspect
 from cinder import exception
 
+def count_initial_spaces(line):
+    counter = 0
+    for i in line:
+       if i == ' ':
+          counter = counter + 1
+       else:
+          return counter
+
 def get_exception_msg(variable_name):
     global line_buffer
-#    variable_name = variable_name +" ="
-    #import pdb;pdb.set_trace();
     msg_buffer = []
     for i in reversed(line_buffer):
-        if variable_name +" =" in i or variable_name +"=" in i:
-#           import pdb; pdb.set_trace();
+        if variable_name +" =" in i or variable_name +"=" in i or variable_name +" ==" in i or variable_name +"==" in i:
            line = i
            line = line.strip()
+           line = line.split(variable_name,1)[-1].replace("=","").strip()
            line = line.replace(variable_name+" = ","")
            line = line.replace(variable_name +"=","")
+           line = line.replace(variable_name+" == ","")
+           line = line.replace(variable_name +"==","")
            line = line.replace("(_(","")
            line = line.replace("_(","")
            line = line.replace(')"',"")
            line = line.replace('"',"")
            line = line.strip()
-           #print line
+           line = line.strip('\'')
+           line = line.strip(')')
+           line = line.strip()
            line_buffer = []
+           for j in reversed(msg_buffer):
+               if count_initial_spaces(j) > count_initial_spaces(i):
+                    j = j.replace(')"',"")
+                    j = j.replace('"',"")
+                    j = j.strip()
+                    j = j.strip('\'')
+                    j = j.strip(')')
+                    j = j.strip()
+                    line = line +" "+j
            return line
-#           for j in reversed(msg_buffer):
-#               print j
         else:
            msg_buffer.append(i)
 
-def parse_exception(line):
+def parse_exception(line,lnumber,fname):
     global except_started
     global exc
     global line_buffer
-#    line_buffer.append(line)
-#    print line
-#    import pdb; pdb.set_trace()
     if except_started:
        if "raise exception" in line:
-#           print line
-#           import pdb;pdb.set_trace();
-#           print line
            match = re.search(r"raise exception.(.*)", line)
            if match:
-#               import pdb; pdb.set_trace();
                line = match.group(1)
                exception_name = line.split("(")[0]
                use_default = False
                try:
                    variable_name = line.split("=")[1].replace(")","")
-   #               import pdb;pdb.set_trace();
                    message = get_exception_msg(variable_name)
                except:
-#                   import pdb; pdb.set_trace();
                    use_default = True
                try:
                     if inspect.isclass(getattr(exception, exception_name)):
                         if not exception_name.startswith('_'):
-#                          print exception
-#                          print exception_name
-#                          if "Error" in exception_name:
-#                              import pdb;pdb.set_trace();
                              if use_default:
                                  return
                                  message = getattr(exception, exception_name).message
@@ -69,13 +73,13 @@ def parse_exception(line):
                              try:
                                  exc.append({'name': exception_name,
                                  'code': getattr(exception, exception_name).code,
-                                 'msg': message})
-                                 #import pdb;pdb.set_trace();
+                                 'msg': message.strip(),
+                                 'line_number':lnumber,
+                                 'file_name':fname})
                              except:
                                  return
                except:
                     return
-                    #import pdb;pdb.set_trace(); 
            return
     line_buffer.append(line)
 
@@ -84,7 +88,6 @@ def parse_repo(clone_url):
     global exc
     global line_buffer
     dir_name = clone_url.split("/")[-1].split(".")[0]
-#    print dir_name
     cmd = "rm -rf "+dir_name
     os.system(cmd)
     cmd = "git clone "+clone_url
@@ -95,12 +98,14 @@ def parse_repo(clone_url):
                suffix = ".py"
                if filename.endswith(suffix):
                   file_path = os.path.join(root, filename)
-                  #print('\t- file %s (full path: %s)' % (filename, file_path))
+#                  print('\t- file %s (full path: %s)' % (filename, file_path))
                   line_buffer = []
-                  #import pdb;pdb.set_trace();
+                  line_number = 1
+                  file_name = file_path
                   with open(file_path) as f:
                       for line in f:
-                          parse_exception(line)
+                          parse_exception(line,line_number,file_name)
+                          line_number = line_number + 1
     cmd = "rm -rf "+dir_name
     os.system(cmd)
 
@@ -108,22 +113,29 @@ def parse_repo(clone_url):
 
 
 clone_url = "https://github.com/openstack/cinder.git"
-dir_name = clone_url.split("/")[-1].split(".")[0]
-cmd = "rm -rf "+dir_name
-#os.system(cmd)
 cmd = "git clone "+clone_url
-#os.system(cmd)
 
 except_started = True
 exc = []
 line_buffer = []
 
 parse_repo(clone_url)
-#import pdb;pdb.set_trace();
+items_set = set()
+exceptions_set = set()
+result = list()
 
-#with open("cinder/cinder/volume/drivers/hitachi/hnas_utils.py") as f:
-#    for line in f:
-#        parse_exception(line) 
-print json.dumps({'cinder': exc})
+for obj in exc:
+    if obj['msg'] is None:
+        if obj['name'] not in exceptions_set:
+             obj['msg'] = 'null'
+        else:
+             continue
+    exceptions_set.add(obj['name'])
+    json_obj = str(obj['code'])+obj['name']+obj['msg']
+    if not json_obj in items_set:
+        items_set.add(json_obj)
+        result.append(obj)
 
-#print exc
+
+print json.dumps({'cinder': result})
+
